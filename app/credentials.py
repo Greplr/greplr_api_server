fe27aa03f13235aa42d69d014901aff5d844a103
Shubham Dokania <skd.1810@gmail.com>
2015-06-11 16:16:15 +0530

    Uber credentials

--- app/credentials.py
+++ app/credentials.py
@@ -1,2 +1,8 @@
 client_id = 'f1098567ba76b80'
 client_secret = '01c8236c4dd894d0ad23eb77ce79f33c3a1d902b'
+
+uber_credentials = {
+    'client_id': 'm7kPJcR3E6vsRL9jSUscJhFH9kVSQqBD',
+    'server_token': '0TZ3-VeOGfHCu9_zTmjeLLiRQQ58-QuosGnbxyH3',
+    'secret': 'kADzIMxf_EJKQfncy1y5PfcRX9ksMtU4zMu7tvIF'
+}
